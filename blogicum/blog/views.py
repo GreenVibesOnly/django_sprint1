c592014c8a229d6c9b4dc9df1bc3b637ad483ef7
Kseniya Tetercheva <snegurka.tkd@gmail.com>
2023-07-24 17:09:12 +0300

    pytest passed

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -47,7 +47,7 @@ posts = [
 
 def index(request):
     template = 'blog/index.html'
-    context = {'posts': posts}
+    context = {'posts': posts[::-1]}
     return render(request, template, context)
 
 
