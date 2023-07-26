d4f50857306721ddbc5f0671e6c5392f1f73d549
Kseniya Tetercheva <snegurka.tkd@gmail.com>
2023-07-26 16:05:37 +0300

    add TemplateView, add IndexError test, upgrade post_model template

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,1 @@
-from django.shortcuts import render
-
-
-def about(request):
-    template = 'pages/about.html'
-    return render(request, template)
-
-
-def rules(request):
-    template = 'pages/rules.html'
-    return render(request, template)
+# here will be views functions
